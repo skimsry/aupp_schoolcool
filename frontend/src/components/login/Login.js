cb404e38a511ea6cbc74cb23e659f87dfc7931d6
sornkimsry <sornkimsry07@gmail.com>
2024-07-03 07:41:56 +0700

    update login page

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -18,15 +18,15 @@ const Login = () => {
                   for="email"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Your email
+                  Your email / Username
                 </label>
                 <input
-                  type="email"
-                  name="email"
-                  id="email"
+                  type="text"
+                  name="eu"
+                  id="eu"
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
-                  required=""
+                  placeholder="name@company.com / sorn.kimsry"
+                  required="true"
                 />
               </div>
               <div>
@@ -42,7 +42,7 @@ const Login = () => {
                   id="password"
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
+                  required="true"
                 />
               </div>
               <div className="flex items-center justify-between">
--- frontend/src/components/signup/Signup.js
+++ frontend/src/components/signup/Signup.js
@@ -71,7 +71,10 @@ const Signup = () => {
   };
 
   return (
-    <section className="text-left bg-gray-50 dark:bg-gray-900">
+    <section
+      className="text-left bg-gray-50 dark:bg-gray-900"
+      style={{ paddingTop: "120px", paddingBottom: "50px" }}
+    >
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
